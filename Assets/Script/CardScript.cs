b30972b77a1daf3754bf3e8fa22f61b20c521b32
Ffaithy <violeta.mrn@gmail.com>
2018-11-20 17:33:59 +0000

    Made Card abstract so that it can't be instantiated

--- Assets/Script/CardScript.cs
+++ Assets/Script/CardScript.cs
@@ -2,18 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardScript : MonoBehaviour {
-
+public abstract class CardScript : MonoBehaviour
+{
     public string Name;
     public int EnergyCost;
     public int DamageAmount;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
--- Assets/Script/CreatureScript.cs
+++ Assets/Script/CreatureScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class CreatureScript : CardScript {
 
     public int Health;

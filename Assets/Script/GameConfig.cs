fd35228867967145d2fb6cb6984207d3f4905c24
Ffaithy <violeta.mrn@gmail.com>
2018-11-21 13:44:48 +0000

    Genrate players decks

--- Assets/Script/GameConfig.cs
+++ Assets/Script/GameConfig.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class GameConfig : MonoBehaviour {
 
+    public static GameObject[] Creatures;
+    public static GameObject[] Attacks;
+
     public int MaxTurns;
     public int MaxNumCardsInDeck;
 
-	// Use this for initialization
-	void Start () {
-		
+    private void Awake()
+    {
+        //Initialize the cards with the available prefabs
+        Creatures = Resources.LoadAll<GameObject>("Creatures");
+        Attacks = Resources.LoadAll<GameObject>("Attacks");
+
+        Debug.Log("Creatures and Attacks initialized");
+    }
+
+    // Use this for initialization
+    void Start () 
+    {
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 		
 	}
+
+    public GameObject GenerateCreature()
+    {
+        return Instantiate(Creatures[Random.Range(0, Creatures.Length)]);
+    }
+
+    public GameObject GenerateAttack()
+    {
+        return Instantiate(Attacks[Random.Range(0, Attacks.Length)]);
+    }
+
 }
 
 // creature list
